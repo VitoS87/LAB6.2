3cb0cf14a03ec25cdafbc47eea92da382324e6f8
Vitos <rustamminis@gmail.com>
2025-11-24 14:04:50 +0200

    Add files via upload

---
+++ LAB6.2/LAB6.2/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    Random rand = new Random();
+    double[,] mas = new double[7, 5];
+    Console.WriteLine("Исходный массив: ");
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i, j] = rand.Next(-9, 10);
+            Console.Write(mas[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine($"Массив суммы отрицательных элементов строк страницы: ");
+    double[] mas1 = MyLib.Matrix.MasStrNegSum(mas);
+    for (int i = 0; i < mas1.GetLength(0); i++)
+    {
+        Console.Write(mas1[i] + " ");
+    }
+    Console.WriteLine();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+
+}
---
+++ LAB6.2/MyLib/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace MyLib
+{
+    public class Matrix
+    {
+        public static double[] MasStrNegSum(double[,] mas)
+        {
+            double[] msns = new double[mas.GetLength(0)];
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                double x = 0;
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    if (mas[i, j] < 0)
+                    {
+                        x += mas[i, j];
+                    }
+                }
+                msns[i] = x;
+            }
+            return msns;
+        }
+    }
+}
